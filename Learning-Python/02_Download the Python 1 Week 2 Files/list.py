d157f93f0a23f8ea98909371594b3836bc9d33e6
LaTrisha <L_gomer@yahoo.com>
2024-02-17 10:23:31 -0800

    python practice files

---
+++ Learning-Python/02_Download the Python 1 Week 2 Files/list.py
@@ -0,0 +1,10 @@
+my_list = ["first", "second", "third", "fourth"]
+
+# print single element using the positive index
+print(my_list[1])
+
+# print single element using the negative index
+print(my_list[-1])
+
+# print range of values using the slice operator
+print(my_list[1:3])
